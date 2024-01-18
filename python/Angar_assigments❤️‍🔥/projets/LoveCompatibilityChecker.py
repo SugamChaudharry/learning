d1b4dc01d0a6eea7adad8af9d0a7b33c4d2b299f
sugam <nextgenachieverss@gmail.com>
2024-01-18 21:29:37 +0530

    Update LoveCompatibilityChecker.py with new score range

--- python/Angar_assigments❤️‍🔥/projets/LoveCompatibilityChecker.py
+++ python/Angar_assigments❤️‍🔥/projets/LoveCompatibilityChecker.py
@@ -7,7 +7,7 @@ score = (len(userName1) + len(userName2)) * (userAge1 + userAge2)
 
 if score < 500:
     print(" papa kehte hai bada naam karega, beta hamara aisa kaam karega ")
-elif 500 <= score <= 200:
+elif 500 <= score <= 1000:
     print(" kuch kuch hota hai ")
 else:
     print(" resta mubaraka ho 😍❤️‍🔥🥳🥳🥳🥳🥳🥳🥳🥳💇💇💇💇💇")
